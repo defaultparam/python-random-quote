3efbf9e9c61b0a7b37f9b165cb5d6e4e30c020a0
Paramjot Singh <theparamjotsingh@gmail.com>
2022-08-31 09:19:59 +0530

    Explored Python on my own

--- get-quote.py
+++ get-quote.py
@@ -3,12 +3,21 @@ import random
 def main():
   print("Keep it logically awesome.")
   print("There's your quote")
+
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+
   last = len(quotes)
-  i = random.randint(0,last)
-  
-  print(quotes[i])
+
+  i = random.randint(0,(last-1))
+  print(quotes[i], end="")
+  i = random.randint(0,(last-1))
+  print(quotes[i], end="")
+
+  quote = input('Add a quote of your own now: ')
+  f = open('quotes.txt', 'a')
+  f.write(quote+'\n')
+  f.close()
 if __name__== "__main__":
   main()
