317ba6948e0bb9b815ed41fd4e12984c9045c01f
Paramjot Singh <theparamjotsingh@gmail.com>
2022-08-31 08:59:20 +0530

    Update #3

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
+
 def main():
   print("Keep it logically awesome.")
   print("There's your quote")
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
-
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes)
+  i = random.randint(0,last)
+  
+  print(quotes[i])
 if __name__== "__main__":
   main()
